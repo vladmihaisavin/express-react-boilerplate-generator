8bcf8839947fbedbf9bd589d6c51c52f0220fd84
Vlad-Mihai Savin <savin.vladmihai@gmail.com>
2020-06-05 20:15:51 +0300

    finished generating client files

--- src/generators/resources/clientGenerators.js
+++ src/generators/resources/clientGenerators.js
@@ -23,7 +23,7 @@ module.exports = ({
     } else {
       content = removeLines(content, [5, 6, 26])
     }
-    
+
     const routeType = hasAuthentication ? 'ProtectedRoute' : 'Route'
     const DashboardComponentCall = `${addTabs(3)}<${routeType} exact path="/dashboard" component={withLayout(Dashboard)} />`
     content = content.replace(/###DashboardComponentCall###/g, DashboardComponentCall)
@@ -46,8 +46,27 @@ module.exports = ({
     writeFile(relativePath, content)
   }
 
-  const generateResourcesSections = (relativePath) => {}
-  const generateResourceFormSections = (relativePath) => {}
+  const generateResourcesSections = (relativePath) => {
+    const resourcesSectionStubContent = readFile(relativePath.replace('Users.jsx', 'Resources.jsx'))
+    resources.forEach(resource => {
+      let content = resourcesSectionStubContent.replace(/###ResourcePlural###/g, resource.ResourcePlural)
+      content = content.replace(/###resourcePlural###/g, resource.resourcePlural)
+      content = content.replace(/###resourceSingular###/g, resource.resourceSingular)
+      
+      writeFile(relativePath.replace('Users.jsx', `${resource.ResourcePlural}.jsx`), content)
+    })
+  }
+
+  const generateResourceFormSections = (relativePath) => {
+    const resourceFormSectionStubContent = readFile(relativePath.replace('UserForm.jsx', 'ResourceForm.jsx'))
+    resources.forEach(resource => {
+      let content = resourceFormSectionStubContent.replace(/###ResourceSingular###/g, resource.ResourceSingular)
+      content = content.replace(/###resourceSingular###/g, resource.resourceSingular)
+      content = content.replace(/###resourcePlural###/g, resource.resourcePlural)
+      
+      writeFile(relativePath.replace('UserForm.jsx', `${resource.ResourceSingular}Form.jsx`), content)
+    })
+  }
 
   const generateHeader = (relativePath) => {
     let content = readFile(relativePath)
@@ -64,9 +83,40 @@ module.exports = ({
     writeFile(relativePath, content)
   }
 
-  const generateNavigator = (relativePath) => {}
-  const generateResourcesServices = (relativePath) => {}
-  const generateResourceResources = (relativePath) => {}
+  const generateNavigator = (relativePath) => {
+    let content = readFile(relativePath)
+
+    const ResourceObjects = resources.map(resource => {
+      return `${addTabs(3)}{ id: '${resource.ResourcePlural}', icon: <PeopleIcon />, active: true, url: '/${resource.resourcePlural}' }`
+    }).join(os.EOL)
+    content = content.replace(/###ResourceObjects###/g, ResourceObjects)
+
+    writeFile(relativePath, content)
+  }
+
+  const generateResourcesServices = (relativePath) => {
+    const resourcesServiceStubContent = readFile(relativePath.replace('users.js', 'resources.js'))
+    resources.forEach(resource => {
+      let content = resourcesServiceStubContent.replace(/###resourcePlural###/g, resource.resourcePlural)
+      content = content.replace(/###resourceSingular###/g, resource.resourceSingular)
+      
+      writeFile(relativePath.replace('users.js', `${resource.resourcePlural}.js`), content)
+    })
+  }
+
+  const generateResourceResources = (relativePath) => {
+    resources.forEach(resource => {
+      const resourceObject = {
+        resourceName: resource.resourceSingular,
+        resourceUrl: resource.resourcePlural,
+        listProperties: [],
+        formProperties: [],
+        formFields: []
+      }
+
+      writeFile(relativePath.replace('userResource.json', `${resource.resourceSingular}Resource.json`), JSON.stringify(resourceObject))
+    })
+  }
 
   const generatePackageJson = (relativePath) => {
     let content = readFile(relativePath)
