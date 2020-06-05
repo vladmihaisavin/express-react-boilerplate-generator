3e3cbee27608de0745eaff0ee22416b9c52cb721
Vlad-Mihai Savin <savin.vladmihai@gmail.com>
2020-06-05 20:34:29 +0300

    WIP generate server files

--- src/generators/resources/serverGenerators.js
+++ src/generators/resources/serverGenerators.js
@@ -9,12 +9,24 @@ module.exports = ({
     writeFile
   } = fileManager
 
-  const generateResourcesControllers = (relativePath) => {}
-  const generateResourceModels = (relativePath) => {}
+  const generateResourcesControllers = (relativePath) => {
+    const resourcesControllerStubContent = readFile(relativePath.replace('users.js', 'resourcesController.js'))
+    resources.forEach(resource => {
+      let content = resourcesControllerStubContent.replace(/###resourcePlural###/g, resource.resourcePlural)
+      content = content.replace(/###resourceSingular###/g, resource.resourceSingular)
+      
+      writeFile(relativePath.replace('users.js', `${resource.resourcePlural}.js`), content)
+    })
+  }
+
+  const generateResourceModels = (relativePath) => {
+    
+  }
   const generateResourceRepositories = (relativePath) => {}
   const generateResourcesValidators = (relativePath) => {}
   const generateRoutes = (relativePath) => {}
   const generateSchemas = (relativePath) => {}
+
   const generatePackageJson = (relativePath) => {
     let content = readFile(relativePath)
     writeFile(relativePath, content.replace(/###projectName###/g, projectName))
